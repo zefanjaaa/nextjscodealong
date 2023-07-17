219734a6718b879c87e4dbb418e23da2bb931751
Zefanja Timorason <z.timorason@gmail.com>
2023-07-18 00:30:08 +0200

    removed some empty rulesets and moved the links array in the NavBar.jsx to a data.js file for cleaner code.

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -3,38 +3,7 @@ import styles from "./navbar.module.css";
 
 import Link from "next/link";
 import React from "react";
-const links = [
-  {
-    id: 1,
-    title: "Home",
-    url: "/",
-  },
-  {
-    id: 2,
-    title: "Portfolio",
-    url: "/portfolio",
-  },
-  {
-    id: 3,
-    title: "Blog",
-    url: "/blog",
-  },
-  {
-    id: 4,
-    title: "About",
-    url: "/about",
-  },
-  {
-    id: 5,
-    title: "Contact",
-    url: "/contact",
-  },
-  {
-    id: 6,
-    title: "Dashboard",
-    url: "/dashboard",
-  },
-];
+import { links } from "./data.js";
 
 const Navbar = () => {
   return (
