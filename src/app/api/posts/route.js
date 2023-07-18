a1b5d897ffd27e002f0d00150c521ce54d11fec0
Zefanja Timorason <z.timorason@gmail.com>
2023-07-18 17:15:22 +0200

    accidently uploaded the .env file

--- src/app/api/posts/route.js
+++ src/app/api/posts/route.js
@@ -1,5 +1,14 @@
 import { NextResponse } from "next/server";
+import connect from "@/utils/db";
+import Post from "@/models/Post";
 
 export const GET = async (req) => {
-  return new NextResponse("it works!", { status: 200 });
+  try {
+    await connect();
+
+    const posts = await Post.find();
+    return new NextResponse(posts, { status: 200 });
+  } catch (error) {
+    return new NextResponse("Database error!", { status: 500 });
+  }
 };
