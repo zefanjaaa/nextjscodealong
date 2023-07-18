59d9fca28f160a3d0bd42b6dabd10dfed55e83fd
Zefanja Timorason <z.timorason@gmail.com>
2023-07-18 16:37:57 +0200

    started on the dashboard, cleaned some pages and installed a new package for data fetching

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 import styles from "./page.module.css";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import useSWR from "swr";
 
 const Dashboard = () => {
+  const session = useSession();
+  const router = useRouter();
+
+  const fetcher = (...args) => fetch(...args).then((res) => res.json());
   return <div>Dashboard</div>;
 };
 
--- src/app/portfolio/[category]/page.jsx
+++ src/app/portfolio/[category]/page.jsx
@@ -2,7 +2,9 @@ import React from "react";
 import styles from "./page.module.css";
 import Button from "@/components/Button/Button";
 import Image from "next/image";
-import { items } from "./data";
+import { items } from "./data.js";
+import { notFound } from "next/navigation";
+
 const getData = (cat) => {
   const data = items[cat];
 
